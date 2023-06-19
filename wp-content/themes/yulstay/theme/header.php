2ebeb6e34bc90c1f681e71dc3c685e24c01475fd
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-06-19 23:28:05 +0530

    setting up

--- wp-content/themes/yulstay/theme/header.php
+++ wp-content/themes/yulstay/theme/header.php
@@ -152,7 +152,7 @@
             
           </div>
             
-            <div class="px-6 py-14 md:p-14 h-screen flex flex-col self-end justify-end hidden md:block">
+            <div class="px-6 py-14 md:p-14 h-screen flex flex-col self-end justify-end">
                 <div class="md:block hidden">
                   <img src="<?php echo get_template_directory_uri(); ?>/assets/img/1.png" class="w-32 pb-4" alt="">
                 </div>
