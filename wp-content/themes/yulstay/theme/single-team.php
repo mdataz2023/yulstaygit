9007226f1b3fb96d587c4eff1f095fe88e0cc147
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-07-17 22:21:33 +0530

    setting up

--- wp-content/themes/yulstay/theme/single-team.php
+++ wp-content/themes/yulstay/theme/single-team.php
@@ -88,8 +88,8 @@ $linkedin_link = get_field('linkedin_link');
      </div>
      <div class="col-span-1 md:col-span-7 col-scroll">
           <div>
-               <h1 class="text-3xl font-bold"><?php the_title(); ?></h1>
-               <p><?php the_excerpt(); ?></p>
+               <h1 class="text-3xl font-bold font-poppins pb-2"><?php the_title(); ?></h1>
+               <p class="font-poppins font-medium text-xl pb-2"><?php the_excerpt(); ?></p>
                <div class="flex flex-col gap-3">
                <?php if( have_rows('paragraph_repeater') ): $s= 1; $a=0; ?>
 				
@@ -99,7 +99,7 @@ $linkedin_link = get_field('linkedin_link');
                       ?>
                     
                     <div>
-                         <p><?php echo $paragraph; ?></p>
+                         <p class="font-poppins font-medium text-base pb-2"><?php echo $paragraph; ?></p>
                     </div>
                     <?php   endwhile; endif; ?>
                </div>
@@ -129,29 +129,29 @@ $linkedin_link = get_field('linkedin_link');
               
 
                <div>
-                    <h4><?php echo $team_member_name; ?></h4>
-                    <p><?php echo $team_member_position; ?></p>
-                    <div class="flex">
+                    <h4 class="font-poppins font-bold text-lg pb-2"><?php echo $team_member_name; ?></h4>
+                    <p class="font-poppins font-medium text-lg pb-3"><?php echo $team_member_position; ?></p>
+                    <div class="flex gap-5 pb-2">
                          <div>
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
                               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
                               </svg>
                          </div>
                          <div>
-                              <a href="<?php echo $mobile_link; ?>"><?php echo $mobile; ?></a>
+                              <a class="font-poppins font-medium text-base" href="<?php echo $mobile_link; ?>"><?php echo $mobile; ?></a>
                          </div>
                     </div>
-                    <div class="flex">
+                    <div class="flex gap-5 pb-2">
                          <div>
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe2" viewBox="0 0 16 16">
                               <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855-.143.268-.276.56-.395.872.705.157 1.472.257 2.282.287V1.077zM4.249 3.539c.142-.384.304-.744.481-1.078a6.7 6.7 0 0 1 .597-.933A7.01 7.01 0 0 0 3.051 3.05c.362.184.763.349 1.198.49zM3.509 7.5c.036-1.07.188-2.087.436-3.008a9.124 9.124 0 0 1-1.565-.667A6.964 6.964 0 0 0 1.018 7.5h2.49zm1.4-2.741a12.344 12.344 0 0 0-.4 2.741H7.5V5.091c-.91-.03-1.783-.145-2.591-.332zM8.5 5.09V7.5h2.99a12.342 12.342 0 0 0-.399-2.741c-.808.187-1.681.301-2.591.332zM4.51 8.5c.035.987.176 1.914.399 2.741A13.612 13.612 0 0 1 7.5 10.91V8.5H4.51zm3.99 0v2.409c.91.03 1.783.145 2.591.332.223-.827.364-1.754.4-2.741H8.5zm-3.282 3.696c.12.312.252.604.395.872.552 1.035 1.218 1.65 1.887 1.855V11.91c-.81.03-1.577.13-2.282.287zm.11 2.276a6.696 6.696 0 0 1-.598-.933 8.853 8.853 0 0 1-.481-1.079 8.38 8.38 0 0 0-1.198.49 7.01 7.01 0 0 0 2.276 1.522zm-1.383-2.964A13.36 13.36 0 0 1 3.508 8.5h-2.49a6.963 6.963 0 0 0 1.362 3.675c.47-.258.995-.482 1.565-.667zm6.728 2.964a7.009 7.009 0 0 0 2.275-1.521 8.376 8.376 0 0 0-1.197-.49 8.853 8.853 0 0 1-.481 1.078 6.688 6.688 0 0 1-.597.933zM8.5 11.909v3.014c.67-.204 1.335-.82 1.887-1.855.143-.268.276-.56.395-.872A12.63 12.63 0 0 0 8.5 11.91zm3.555-.401c.57.185 1.095.409 1.565.667A6.963 6.963 0 0 0 14.982 8.5h-2.49a13.36 13.36 0 0 1-.437 3.008zM14.982 7.5a6.963 6.963 0 0 0-1.362-3.675c-.47.258-.995.482-1.565.667.248.92.4 1.938.437 3.008h2.49zM11.27 2.461c.177.334.339.694.482 1.078a8.368 8.368 0 0 0 1.196-.49 7.01 7.01 0 0 0-2.275-1.52c.218.283.418.597.597.932zm-.488 1.343a7.765 7.765 0 0 0-.395-.872C9.835 1.897 9.17 1.282 8.5 1.077V4.09c.81-.03 1.577-.13 2.282-.287z"/>
                               </svg>
                          </div>
                          <div>
-                              <a href="<?php echo $website_link; ?>"><?php echo $website; ?></a>
+                              <a class="font-poppins font-medium text-base" href="<?php echo $website_link; ?>"><?php echo $website; ?></a>
                          </div>
                     </div>
-                    <div class="flex">
+                    <div class="flex gap-5 pb-2">
                          <div>
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
                               <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
@@ -159,10 +159,10 @@ $linkedin_link = get_field('linkedin_link');
                               </svg>
                          </div>
                          <div>
-                              <a href="<?php echo $home_address_link; ?>"><?php echo $home_address; ?></a>
+                              <a class="font-poppins font-medium text-base" href="<?php echo $home_address_link; ?>"><?php echo $home_address; ?></a>
                          </div>
                     </div>
-                    <div class="flex">
+                    <div class="flex gap-5 pb-3">
                          <div>
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-buildings" viewBox="0 0 16 16">
                               <path d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022ZM6 8.694 1 10.36V15h5V8.694ZM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15Z"/>
@@ -170,10 +170,10 @@ $linkedin_link = get_field('linkedin_link');
                               </svg>
                          </div>
                          <div>
-                              <a href="<?php echo $office_address_link; ?>"><?php echo $office_address; ?></a>
+                              <a class="font-poppins font-medium text-base" href="<?php echo $office_address_link; ?>"><?php echo $office_address; ?></a>
                          </div>
                     </div>
-                    <div class="flex">
+                    <div class="flex gap-11">
                          <div>
                               <a href="<?php echo $facebook_link; ?>">
                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
