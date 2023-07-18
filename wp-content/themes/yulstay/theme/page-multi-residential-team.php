20795cf75743181c30eea8da3a970e09364974f2
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-07-18 16:21:12 +0530

    setting up

--- wp-content/themes/yulstay/theme/page-multi-residential-team.php
+++ wp-content/themes/yulstay/theme/page-multi-residential-team.php
@@ -49,8 +49,8 @@ $team_page_description = get_field('team_page_description');
             $categories = get_the_category();
             ?>
             
-            <div>
-            <a class="hover:bg-gray-400" href="<?php the_permalink(); ?>">
+            <div class="hover:bg-gray-400">
+            <a href="<?php the_permalink(); ?>">
                 <?php
                 if (has_post_thumbnail()) {
                     $attachment_image = wp_get_attachment_url($thumbnail_id);
--- wp-content/themes/yulstay/theme/single-team.php
+++ wp-content/themes/yulstay/theme/single-team.php
@@ -72,14 +72,14 @@ $linkedin_link = get_field('linkedin_link');
 						</svg>
 					</div>
 				</div>
-				<div><a href="<?php bloginfo('url'); ?>/home" class="text-gray-400 hover:text-gray-800 font-bold"><?php the_title(); ?></a></div>
+				<!-- <div><a href="<?php bloginfo('url'); ?>/home" class="text-gray-400 hover:text-gray-800 font-bold">Info</a></div>
                     <div class="flex items-center content-between">
 					<div>
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
 						<path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
 						</svg>
 					</div>
-				</div>
+				</div> -->
 				<div class="font-medium"><?php the_title(); ?></div>
 			</div>
                <div>
