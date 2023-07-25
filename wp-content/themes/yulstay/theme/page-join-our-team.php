573f9962e3cd996451e8e1d2fec682bcd689c5e8
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-07-25 05:38:39 +0530

    setting up

--- wp-content/themes/yulstay/theme/page-join-our-team.php
+++ wp-content/themes/yulstay/theme/page-join-our-team.php
@@ -47,6 +47,19 @@ $email = get_field('email');
       <h2 class="text-3xl font-bold tracking-tight text-black"><?php echo $join_our_team_page_title; ?></h2>
 
       <p class="mt-6 text-lg leading-8 text-black"><?php echo $join_our_team_page_description; ?></p>
+      <div class="flex gap-2">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#00AEC5" class="bi bi-plus" viewBox="0 0 16 16">
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+          </svg>
+        </div>
+        <div>
+          <p class="mt-6 text-lg leading-8 text-black">Test</p>
+        </div>
+        
+      </div>
+      
+      <p class="mt-6 text-lg leading-8 text-black">Test test</p>
 
       <div class="grid grid-cols-2">
 
