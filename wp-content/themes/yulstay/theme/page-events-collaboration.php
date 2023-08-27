9414aa4a9018d48fabbcbe0dacfbb6cc356ad0cb
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-08-28 02:46:44 +0530

    Test

--- wp-content/themes/yulstay/theme/page-events-collaboration.php
+++ wp-content/themes/yulstay/theme/page-events-collaboration.php
@@ -293,76 +293,8 @@ button:active {
 
   </div>
   </div>
-
-  <div class="center1">
-          <button class="btn1">
-            <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
-              <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
-              <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
-            </svg>
-            <span class="span1">HOVER ME</span>
-          </button>
-        </div>
-
-      <style>
-        @import url('https://fonts.googleapis.com/css?family=Lato:100&display=swap');
-
-
-
-.container {
-  width: 400px;
-  height: 400px;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.center1 {
-  width: 180px;
-  height: 60px;
-  position: absolute;
-}
-
-.btn1 {
-  width: 180px;
-  height: 60px;
-  cursor: pointer;
-  background: transparent;
-  border: 1px solid #00AEC5;
-  outline: none;
-  transition: 1s ease-in-out;
-}
-
-svg {
-  position: absolute;
-  left: 0;
-  top: 0;
-  fill: none;
-  stroke: #00AEC5;
-  stroke-dasharray: 150 480;
-  stroke-dashoffset: 150;
-  transition: 1s ease-in-out;
-}
-
-.btn1:hover {
-  transition: 1s ease-in-out;
-  background: #ffffff;
-}
-
-.btn1:hover svg {
-  stroke-dashoffset: -480;
-}
-
-.btn1 .span1 {
-  color: #00AEC5;
-  font-size: 18px;
-  font-weight: 100;
-}
-</style>
+  
+  
 </div>
 
 
