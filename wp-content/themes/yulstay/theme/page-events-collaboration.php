3a24b5e22c741f98fcf531eb0d6f87e50bf740cf
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-08-28 02:40:53 +0530

    Tewst

--- wp-content/themes/yulstay/theme/page-events-collaboration.php
+++ wp-content/themes/yulstay/theme/page-events-collaboration.php
@@ -293,10 +293,8 @@ button:active {
 
   </div>
   </div>
-  
-</div>
 
-<div class="center">
+  <div class="center">
           <button class="btn">
             <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
               <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
@@ -364,5 +362,9 @@ svg {
   font-size: 18px;
   font-weight: 100;
 }
+  
+</div>
+
+
 
 <?php get_footer(); ?>
