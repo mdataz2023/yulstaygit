5604db0cfea463262da306b6af6b4f126592a16d
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-08-28 16:43:23 +0530

    btn

--- wp-content/themes/yulstay/theme/page-podcasts.php
+++ wp-content/themes/yulstay/theme/page-podcasts.php
@@ -10,6 +10,75 @@ get_header();
   <div class="text-4xl">
     Coming soon...
   </div>
+
+  <div class="center">
+          <button class="btn">
+            <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+              <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+              <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+            </svg>
+            <span>HOVER ME</span>
+          </button>
+        </div>
+
+      <style>
+        @import url('https://fonts.googleapis.com/css?family=Lato:100&display=swap');
+
+
+.container {
+  width: 400px;
+  height: 400px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.center {
+  width: 180px;
+  height: 60px;
+  position: absolute;
+}
+
+.btn {
+  width: 180px;
+  height: 60px;
+  cursor: pointer;
+  background: transparent;
+  border: 1px solid #00AEC5;
+  outline: none;
+  transition: 1s ease-in-out;
+}
+
+svg {
+  position: absolute;
+  left: 0;
+  top: 0;
+  fill: none;
+  stroke: #00AEC5;
+  stroke-dasharray: 150 480;
+  stroke-dashoffset: 150;
+  transition: 1s ease-in-out;
+}
+
+.btn:hover {
+  transition: 1s ease-in-out;
+  background: #ffffff;
+}
+
+.btn:hover svg {
+  stroke-dashoffset: -480;
+}
+
+.btn span {
+  color: #00AEC5;
+  font-size: 18px;
+  font-weight: 100;
+}
+      </style>
   
 </div>
 
