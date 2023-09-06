baf2eccee15872ff1db56d0e7d8f8a3c369fcc5e
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-09-06 23:34:56 +0530

    Lang

--- wp-content/themes/yulstay/theme/header.php
+++ wp-content/themes/yulstay/theme/header.php
@@ -230,7 +230,7 @@
                                    </svg>
                     </a>
                     <div>
-                      <!-- <?php
+                      <?php
                       if (has_nav_menu('main-menu')) {
                           wp_nav_menu(
                               array(
@@ -239,55 +239,8 @@
                               )
                           );
                       }
-                      ?> -->
-                          <!-- <?php 
-														$lang = get_bloginfo("language"); 
-														if ($lang == 'en-US') { ?>
-															English
-															<?php } elseif($lang == 'fr_FR') {?>
-															French
-													<?php }?>
-
-                          <?php wp_nav_menu( array( 
-															'theme_location' => 'small-menu-lang', 
-															'menu_class' => '', 
-															'container'      => '',
-															'items_wrap'    => '%3$s',
-														) );
-													?>  -->
-
-                          <!-- -------------- -->
-
-                          <div class="relative inline-block text-left">
-                            <div>
-                              <button type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="language-menu-button" aria-expanded="true" aria-haspopup="true">
-                                <?php
-                                $lang = get_bloginfo("language");
-                                if ($lang == 'en-US') {
-                                  echo 'English';
-                                } elseif ($lang == 'fr_FR') {
-                                  echo 'French';
-                                }
-                                ?>
-                                <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                  <path fill-rule="evenodd" d="M6.293 5.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                              </button>
-                            </div>
-
-                            <div class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="language-menu-button" tabindex="-1">
-                              <div class="py-1" role="none">
-                                <?php
-                                wp_nav_menu(array(
-                                  'theme_location' => 'small-menu-lang',
-                                  'menu_class' => 'mt-2 space-y-1',
-                                  'container' => '',
-                                  'items_wrap' => '%3$s',
-                                ));
-                                ?>
-                              </div>
-                            </div>
-                          </div>
+                      ?>
+                          
 
                     </div>
                   </div>
