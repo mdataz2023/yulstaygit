05b6ad953e38e191e52e51d5f463d2672fd03a31
unknown <dulan.zincat@gmail.com>
2023-09-08 02:04:53 +0530

    Map config re

--- wp-content/themes/yulstay/theme/page-residential.php
+++ wp-content/themes/yulstay/theme/page-residential.php
@@ -14,10 +14,10 @@ if ($lang == 'en-US'){
    $language="F";
 }
 ?>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/js-marker-clusterer/1.0.0/markerclusterer_compiled.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyALbrsESmR55p0PLDbeL-SQ_YPuRpucYrw"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/js-marker-clusterer/1.0.0/markerclusterer_compiled.js"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <script  >(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-        ({key: "AIzaSyALbrsESmR55p0PLDbeL-SQ_YPuRpucYrw", v: "weekly"});</script>
+
 <style>
 
 
@@ -399,7 +399,7 @@ async function initMap() {
     const label = labels[i % labels.length];
     const pinGlyph = new google.maps.marker.PinElement({
       glyph: label,
-      glyphColor: "white",
+      glyphColor: "black",
     });
     const marker = new google.maps.marker.AdvancedMarkerElement({
       position,
@@ -420,8 +420,13 @@ async function initMap() {
 }
 
 initMap();
-</script>
 
+// ===================
+
+
+</script>
+<script  >(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "AIzaSyALbrsESmR55p0PLDbeL-SQ_YPuRpucYrw", v: "weekly"});</script>
 <!-- <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyALbrsESmR55p0PLDbeL-SQ_YPuRpucYrw&callback=initMap"> -->
 </script>
