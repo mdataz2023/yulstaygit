d02d418f1ff533d8a862847980d477a75ba05334
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-09-08 12:19:20 +0530

    Test

--- wp-content/themes/yulstay/theme/page-residential.php
+++ wp-content/themes/yulstay/theme/page-residential.php
@@ -26,8 +26,8 @@ if ($lang == 'en-US'){
     /* Change to your desired active button background color */
 }
 </style>
-<div class="h-screen w-screen pt-12 md:pr-12 overflow-y-scroll scroll-style">
-    <div class="pt-20">
+<div class="h-screen w-screen pt-12 md:pt-0 md:pr-12 overflow-y-scroll scroll-style">
+    <div class="pt-0 md:pt-20">
         <!-- <div class="flex pb-7 overflow-hidden gap-1 h-[500px] max-w-7xl mx-auto">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d36553983.44087083!2d-96!3d56!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4b0d03d337cc6ad9%3A0x9968b72aa2438fa5!2sCanada!5e0!3m2!1sen!2slk!4v1692665654783!5m2!1sen!2slk"
@@ -200,7 +200,7 @@ if ($the_query->have_posts()) {
 
     ?>
 
-                <div id="gridView" class="grid grid-cols-1 md:grid-cols-3 gap-x-4 gap-y-5 font-poppins">
+                <div id="gridView" class="grid grid-cols-1 md:grid-cols-3 gap-x-4 gap-y-5 font-poppins px-3 md:px-0">
                     <?php
                 while ($the_query->have_posts()) {
                     $inscriptionsData = $wpdb->get_row(" SELECT * FROM INSCRIPTIONS where NO_INSCRIPTION = '".get_the_content()."'", OBJECT );
